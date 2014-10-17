c0f729c46f9ad2093071eba058cdcd028b29dfa0
amit-mittal <amit.mittal1993@gmail.com>
2014-10-18 00:07:34 +0530

    checking membership of courses in the course groups implemented

--- timetable.pl
+++ timetable.pl
@@ -1,13 +1,25 @@
-%Utility Functions
+% Count number of occurrence of course in the list
 count_course(_, [], 0) :- !.
 count_course(X, [alloted(X, _, _)|T], N) :- count_course(X, T, N2), N is N2 + 1.
 count_course(X, [alloted(Y, _, _)|T], N) :- X \= Y, count_course(X, T, N).
 
+% Count number of occurrence of room and slot together in the list
 count_room_slot(_, _, [], 0) :- !.
 count_room_slot(X, Y, [alloted(_, X, Y)|T], N) :- count_room_slot(X, Y, T, N2), N is N2 + 1.
 count_room_slot(X, Y, [alloted(_, Z, Y)|T], N) :- X \= Z, count_room_slot(X, Y, T, N).
 count_room_slot(X, Y, [alloted(_, X, Z)|T], N) :- Y \= Z, count_room_slot(X, Y, T, N).
 count_room_slot(X, Y, [alloted(_, A, B)|T], N) :- X \= A, Y \= B, count_room_slot(X, Y, T, N).
+
+% Check if a course is in the course group
+if_in_group(Course, [Course|Y]).
+if_in_group(Course, [X|Y]):- X \= Course, if_in_group(Course, Y).
+
+% Check if both courses are in the course group
+if_both_in_group(Course1, Course2, X):- if_in_group(Course1, X), if_in_group(Course2, X).
+
+% Check if two courses are in the same group
+if_in_same_group(Course1, Course2, [course_group(X)|Y]):- if_both_in_group(Course1, Course2, X), !.
+if_in_same_group(Course1, Course2, [course_group(X)|Y]):- if_in_same_group(Course1, Course2, Y).
 
 % course(no., students, faculty_code, no. of lectures)
 course(ma121, 22, kk11, 2).
@@ -19,15 +31,15 @@ room(1101, 30).
 room(1102, 30).
 
 % course_groups(list)
-course_groups([ma121, ma122, ma123]).
+% course_groups([ma121, ma122, ma123]).
 
 % force(course, slots(list))
-force(ma121, slots([a, b, c])).
+force(ma121, slots([a])).
 force(ma122, slots([a, b, c])).
 force(ma123, slots([a, b, c])).
 
 % force(course, rooms(list))
-force(ma121, rooms([1101, 1102])).
+force(ma121, rooms([1101])).
 force(ma122, rooms([1101, 1102])).
 force(ma123, rooms([1101, 1102])).
 
@@ -45,7 +57,7 @@ consistency_check(course_group([CourseCode|B]), X):-
 								count_room_slot(Room, Slot, X, Count2), Count2 == 1,
 								consistency_check(course_group(B), X).
 
-% table contains alloted(course, room no., slot)
+% Checking consistency of one course with respect to the allotment
 solve(CourseCode, alloted(CourseCode, Room, Slot)):- 
 								atom(CourseCode), 
 								course(CourseCode, Students, _, _), 
@@ -57,11 +69,13 @@ solve(CourseCode, alloted(CourseCode, Room, Slot)):-
 								member(Slot, Slots),
 								slot(Slot).
 
-% in the end do consistency check
+% Checking consistency of one course group
 solve(course_group([]), []).
 solve(course_group([CourseCode|B]), [alloted(CourseCode, R, S)|X]):- 
 								solve(A, alloted(CourseCode, R, S)), 
 								solve(course_group(B), X),
 								consistency_check(course_group([CourseCode|B]), [alloted(CourseCode, R, S)|X]).
 
-% Can use delete to delete free slot or room lists
+
+% TODO use no. of lectures needed for that
+% TODO if 2 courses in same group their slot count should also be 1
