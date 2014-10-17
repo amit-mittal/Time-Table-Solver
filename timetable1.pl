519b45d3774d4ec83f14298b9ef47b79137fd4cf
amit-mittal <amit.mittal1993@gmail.com>
2014-10-18 00:08:03 +0530

    old version files added

---
+++ timetable1.pl
@@ -0,0 +1,46 @@
+% course(no., students, faculty_code, no. of lectures)
+course(ma121, 22, kk11, 2).
+course(ma122, 25, kk12, 2).
+course(ma123, 4, kk13, 2).
+
+% room(no., capacity)
+room(1101, 30).
+room(1102, 30).
+
+% course_groups(list)
+course_groups([ma121, ma122, ma123]).
+
+% force(course, slots(list))
+force(ma121, slots([a, b, c])).
+force(ma122, slots([a, b, c])).
+force(ma123, slots([a, b, c])).
+
+% force(course, rooms(list))
+force(ma121, rooms([1101, 1102])).
+force(ma122, rooms([1101, 1102])).
+force(ma123, rooms([1101, 1102])).
+
+% TODO add days to slot info or no. of lectures
+slot(a).
+slot(b).
+slot(c).
+
+% table contains alloted(course, room no., slot)
+solve(CourseCode, [alloted(CourseCode, Room, Slot)|X]):- atom(CourseCode), 
+								course(CourseCode, Students, _, _), 
+								room(Room, Capacity), 
+								slot(Slot),
+								Capacity >= Students,
+								force(CourseCode, rooms(Rooms)),
+								member(Room, Rooms),
+								force(CourseCode, slots(Slots)),
+								member(Slot, Slots),
+								\+ member(alloted(Other, Room, Slot), X),
+								!.
+solve(CourseCode, [alloted(B, _, _)|X]):- atom(CourseCode), solve(CourseCode, X).
+
+% TODO Also implement one solver which takes in whole list
+solve(course_group([]), Table).
+solve(course_group([A|B]), Table):- solve(A, Table), solve(course_group(B), Table).
+
+% Can use delete to delete free slot or room lists
---
+++ timetable2.pl
@@ -0,0 +1,68 @@
+% Count number of occurrence of course in the list
+count_course(_, [], 0) :- !.
+count_course(X, [alloted(X, _, _)|T], N) :- count_course(X, T, N2), N is N2 + 1.
+count_course(X, [alloted(Y, _, _)|T], N) :- X \= Y, count_course(X, T, N).
+
+% Count number of occurrence of room and slot together in the list
+count_room_slot(_, _, [], 0) :- !.
+count_room_slot(X, Y, [alloted(_, X, Y)|T], N) :- count_room_slot(X, Y, T, N2), N is N2 + 1.
+count_room_slot(X, Y, [alloted(_, Z, Y)|T], N) :- X \= Z, count_room_slot(X, Y, T, N).
+count_room_slot(X, Y, [alloted(_, X, Z)|T], N) :- Y \= Z, count_room_slot(X, Y, T, N).
+count_room_slot(X, Y, [alloted(_, A, B)|T], N) :- X \= A, Y \= B, count_room_slot(X, Y, T, N).
+
+% course(no., students, faculty_code, no. of lectures)
+course(ma121, 22, kk11, 2).
+course(ma122, 25, kk12, 2).
+course(ma123, 4, kk13, 2).
+
+% room(no., capacity)
+room(1101, 30).
+room(1102, 30).
+
+% course_groups(list)
+course_groups([ma121, ma122, ma123]).
+
+% force(course, slots(list))
+force(ma121, slots([a])).
+force(ma122, slots([a, b, c])).
+force(ma123, slots([a, b, c])).
+
+% force(course, rooms(list))
+force(ma121, rooms([1101])).
+force(ma122, rooms([1101, 1102])).
+force(ma123, rooms([1101, 1102])).
+
+% TODO add days to slot info or no. of lectures
+slot(a).
+slot(b).
+slot(c).
+
+% Consistency Check
+% just check that room and slot are not getting repeating simultaneously
+consistency_check(course_group([]), X).
+consistency_check(course_group([CourseCode|B]), X):- 
+								member(alloted(CourseCode, Room, Slot), X),
+								count_course(CourseCode, X, Count1), Count1 == 1,
+								count_room_slot(Room, Slot, X, Count2), Count2 == 1,
+								consistency_check(course_group(B), X).
+
+% table contains alloted(course, room no., slot)
+solve(CourseCode, alloted(CourseCode, Room, Slot)):- 
+								atom(CourseCode), 
+								course(CourseCode, Students, _, _), 
+								force(CourseCode, rooms(Rooms)),
+								force(CourseCode, slots(Slots)),
+								member(Room, Rooms),
+								room(Room, Capacity), 
+								Capacity >= Students,
+								member(Slot, Slots),
+								slot(Slot).
+
+solve(course_group([]), []).
+solve(course_group([CourseCode|B]), [alloted(CourseCode, R, S)|X]):- 
+								solve(A, alloted(CourseCode, R, S)), 
+								solve(course_group(B), X),
+								consistency_check(course_group([CourseCode|B]), [alloted(CourseCode, R, S)|X]).
+
+
+% TODO use no. of lectures needed for that
